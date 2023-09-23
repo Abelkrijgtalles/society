377bfe23c89c41f6a345d74dd37e176f0c1522ed
Abel <abelvanhulst@gmail.com>
2023-09-24 00:26:00 +0200

    init

---
+++ src/main/java/nl/abelkrijgtalles/society/Society.java
@@ -0,0 +1,17 @@
+package nl.abelkrijgtalles.society;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Society extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
